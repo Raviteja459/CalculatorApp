52efb33286d750636b454a240141e9333f93f593
rohit prudhvi <ravitejareddyk555@gmail.com>
2021-05-27 18:06:24 +0530

    Initial commit

---
+++ app/src/main/java/com/firstapp/calculatorapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.firstapp.calculatorapp;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void onDigit(View view) {
+    }
+
+    public void onOperator(View view) {
+    }
+
+    public void onDecimalPoint(View view) {
+    }
+
+    public void onClear(View view) {
+    }
+
+    public void onEqual(View view) {
+    }
+}
